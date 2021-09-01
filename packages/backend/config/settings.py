888fcf1bb489f3c4b660ecae6822271380cd6679
yunishimura0716 <yunishimura0716@gmail.com>
2021-08-31 22:03:10 -0700

    added proxy settings

--- packages/backend/config/settings.py
+++ packages/backend/config/settings.py
@@ -42,12 +42,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
     'user',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -137,3 +139,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 
 AUTH_USER_MODEL = 'user.User'
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost:3000',
+)
